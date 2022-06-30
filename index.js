01062a9840559403dcfc25361a33bf34824439e3
Phil Branch <philbranch243@gmail.com>
2022-06-30 18:01:50 -0500

    working with starter code, will continue later, but need something workable to turn in, so I am branching

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+//declare content variable so b/c I got an error saying content was not
+//defined at line 31
+var content = "";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What languages do you know?',
+        choices: [
+            'English', 'Javascript', 'HTML', 'CSS', 'Italian'
+        ]
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    content = `## ${data.name}
+    Languages: ${data.languages}`
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data)=>{
+        writeToFile("./README.md", content);
+    });
+}
 
 // Function call to initialize app
 init();
